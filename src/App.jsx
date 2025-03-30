fb1cab9b7f352dcd9401b4d238c79a7b38eb91d0
emarishkin <ee.marishkin@yandex.ru>
2025-03-30 18:40:15 +0400

    2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import AppMain from './components/AppMain'
 function App() {
   return (
     <>
-      {/* <AppHeader />
-      <AppMain /> */}
+      <AppHeader />
+      {/* <AppMain /> */}
     </>
   )
 }
